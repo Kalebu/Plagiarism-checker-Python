2cb8bb778e0db1902fbb47519b7c335a6db55d0c
Manvitha roy <manvitharoyal110@gmail.com>
2022-05-17 16:18:56 +0530

    comit

--- app.py
+++ app.py
@@ -7,8 +7,10 @@ student_notes = [open(_file, encoding='utf-8').read()
                  for _file in student_files]
 
 
-def vectorize(Text): return TfidfVectorizer().fit_transform(Text).toarray()
-def similarity(doc1, doc2): return cosine_similarity([doc1, doc2])
+def vectorize(Text): 
+    return TfidfVectorizer().fit_transform(Text).toarray()
+def similarity(doc1, doc2): 
+    return cosine_similarity([doc1, doc2])
 
 
 vectors = vectorize(student_notes)
