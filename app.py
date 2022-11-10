979fac03aaf6412a57aa0b2cb8ceb4b8c75f439e
ShivangiTripathi21 <tripathishivi83@gmail.com>
2022-11-10 23:30:08 +0530

    Revert "Todo"
    This reverts commit 78cde4bc6b6f9d84e351df83b2fe1f7c29338830.

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ def vectorize(Text): return TfidfVectorizer().fit_transform(Text).toarray()
 def similarity(doc1, doc2): return cosine_similarity([doc1, doc2])
 
 
-vectors_1 = vectorize(student_notes)
-s_vectors = list(zip(student_files, vectors_1))
+vectors = vectorize(student_notes)
+s_vectors = list(zip(student_files, vectors))
 plagiarism_results = set()
 
 
